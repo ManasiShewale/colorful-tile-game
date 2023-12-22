45f98b64f444d8f102a6b24668df43bb331b0455
manasishewale <manasishewale9@gmail.com>
2023-12-22 20:07:40 +0530

    commit and sync

--- server/index.js
+++ server/index.js
@@ -12,60 +12,61 @@ app.use(bodyParser.json());
 
 // Generate a random grid with red and blue tiles
 function generateGrid() {
-  const colors = ["blue"];
-  const grid = Array.from(
-    {
-      length: 2,
-    },
-    () =>
-      Array.from(
-        {
-          length: 10,
+    const colors = ["blue"];
+    const grid = Array.from({
+            length: 2,
         },
-        () => colors[Math.floor(Math.random() * colors.length)]
-      )
-  );
-  return grid;
+        () =>
+        Array.from({
+                length: 10,
+            },
+            () => colors[Math.floor(Math.random() * colors.length)]
+        )
+    );
+    return grid;
 }
 
 let currentGrid = generateGrid();
 
 app.get("/grid", (req, res) => {
-  res.json({
-    grid: currentGrid,
-  });
+    res.json({
+        grid: currentGrid,
+    });
 });
 
 // Update the /click endpoint
 app.post("/click", (req, res) => {
-  const { row, col } = req.body;
+    const {
+        row,
+        col
+    } = req.body;
 
-  if (currentGrid[row] && currentGrid[row][col]) {
-    const clickedTile = currentGrid[row][col];
-    let scoreChange = 0;
+    if (currentGrid[row] && currentGrid[row][col]) {
+        const clickedTile = currentGrid[row][col];
+        let scoreChange = 0;
 
-    if (clickedTile === "blue") {
-      scoreChange = 10;
+        if (clickedTile === "blue") {
+            scoreChange = 10;
+        }
+
+        // Reset the clicked tile to its original color
+        currentGrid[row][col] = clickedTile;
+
+        res.json({
+            scoreChange,
+        });
+    } else {
+        console.log("Invalid click coordinates");
+        res.status(400).json({
+            error: "Invalid click coordinates",
+        });
     }
-
-    // Reset the clicked tile to its original color
-    currentGrid[row][col] = clickedTile;
-
-    res.json({
-      scoreChange,
-    });
-  } else {
-    console.log("Invalid click coordinates");
-    res.status(400).json({
-      error: "Invalid click coordinates",
-    });
-  }
 });
 
 app.use((req, res) => {
-  res.status(404).send("Not Found");
+    res.status(404).send("Not Found");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
